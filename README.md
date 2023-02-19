16b5461ecdbef646fc51d427ea3865aaf507bfb7
Subash <subash.v@pinnacleseven.com>
2023-02-19 11:12:11 +0530

    [UPDATE] Detect and Crop images from given folder

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Step 1 => Create virtual env
 Step 2 => Activate virtual env
 Step 3 => Install the dependencies 'pip install -r requirements.txt'
-Step 4 => Command for run "python yolo_img_test.py -i 0007.jpg -o test_output_best1_416.jpg
+Step 4 => Command for run "python yolo_img_test.py -i img_folder"
 "
  * Its returning the array of key pair value of detected co-ordinates
  * For reference find the Saved image in the name of test_output_best1_416.jpg
--- yolo_img_test.py
+++ yolo_img_test.py
@@ -1,15 +1,16 @@
 import argparse
 import time
 import glob
-
+import os
 import cv2
 import numpy as np
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", type=str, default="",
-                    help="path to (optional) input image file")
+                    help="path to (optional) input images directory")
 parser.add_argument("-o", "--output", type=str, default="",
-                    help="path to (optional) output image file. Write only the name, without extension.")
+                    help="path to (optional) output image directory")
 parser.add_argument("-c", "--confidence", type=float, default=0.5,
                     help="minimum probability to filter weak detections")
 parser.add_argument("-t", "--threshold", type=float, default=0.4,
@@ -19,13 +20,14 @@ args = vars(parser.parse_args())
 
 CONFIDENCE_THRESHOLD = args["confidence"]
 NMS_THRESHOLD = args["threshold"]
-impath = args["input"]
+path_to_folder = args["input"]
 
 weights = glob.glob("models/*.weights")[0]
 labels = glob.glob("models/*.txt")[0]
 cfg = glob.glob("models/*.cfg")[0]
 
 print("You are now using {} weights ,{} configs and {} labels.".format(weights, cfg, labels))
+start = datetime.now()
 
 lbls = list()
 with open(labels, "r") as f:
@@ -52,7 +54,7 @@ def detect(imgpath, nn):
     layer_outs = nn.forward(layer)
     end_time = time.time()
     coords = []
-
+    crp_imgs = []
     boxes = list()
     confidences = list()
     class_ids = list()
@@ -80,23 +82,38 @@ def detect(imgpath, nn):
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
 
-            color = [int(c) for c in COLORS[class_ids[i]]]
+            # color = [int(c) for c in COLORS[class_ids[i]]]
             crop_img = img[y:y + h, x:x + w]
+            crp_imgs.append(crop_img)
             coords.append({'x1': x, 'x2': x + w, 'y1': y, 'y2': y + h})
-            cv2.imshow("cropped", crop_img)
-            cv2.waitKey(400)
-            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-            text = "{}: {:.4f}".format(lbls[class_ids[i]], confidences[i])
-            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-            label = "Inference Time: {:.2f} ms".format(end_time - start_time)
-            cv2.putText(image, label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            # cv2.imshow("cropped", crop_img)
+            # cv2.waitKey(400)
+            # cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            # text = "{}: {:.4f}".format(lbls[class_ids[i]], confidences[i])
+            # cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            # label = "Inference Time: {:.2f} ms".format(end_time - start_time)
+            # cv2.putText(image, label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
-    cv2.imshow("image", image)
-    if args["output"] != "":
-        cv2.imwrite(args["output"], image)
-    cv2.waitKey(500)
-    return coords
+    # cv2.imshow("image", image)
+    # if args["output"] != "":
+    #     cv2.imwrite(args["output"], image)
+    # cv2.waitKey(500)
+    return coords,crp_imgs
 
+# print(bounding_boxes)
 
-bounding_boxes = detect(impath, net)
-print(bounding_boxes)
+current_dir = os.getcwd()
+if os.path.isdir(os.path.join(current_dir,path_to_folder)):
+    img_list = [itm for itm in os.listdir(os.path.join(current_dir,path_to_folder)) if itm[-4:] in (".jpg",".jpeg",".png") ]
+    for img_path in img_list:
+        bounding_boxes,crp_imgs = detect(os.path.join(current_dir,args['input'],img_path), net)
+        count = 0
+        for crp_img in crp_imgs:
+            count += 1
+            if not os.path.isdir(os.path.join(current_dir,args['input'],img_path[:-4])):
+                os.makedirs(os.path.join(current_dir,args['input'],img_path[:-4]))
+            cv2.imwrite(os.path.join(current_dir,args['input'],img_path[:-4],img_path[:-4]+f"_cropped_{count:04d}"+img_path[-4:]),crp_img)
+
+end = datetime.now()
+
+print(f"Total time has taken to process is {end - start} for count of {count} cropped images!")
